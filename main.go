dff18c142c163abc0e038ec6ac1b2c3850e0f943
jacobrluttrull <jacobrluttrull@gmail.com>
2026-07-31 10:42:21 -0400

    added the apiConfig struct along with middleware wrapper and metrics functions

--- main.go
+++ main.go
@@ -1,17 +1,45 @@
 package main
 
 import "net/http"
+import "sync/atomic"
+import "fmt"
 
+type apiConfig struct {
+    fileserverHits atomic.Int32    
+}
+
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+func (cfg *apiConfig) fileserverHitsHandler(w http.ResponseWriter, r *http.Request) {
+       w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+       w.WriteHeader(http.StatusOK)
+       w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+   }
+func (cfg *apiConfig) resetFileserverHitsHandler(w http.ResponseWriter, r *http.Request) {
+         cfg.fileserverHits.Store(0)
+         w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+         w.WriteHeader(http.StatusOK)
+         w.Write([]byte("Hits counter reset"))
+    }    
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	apiCfg := &apiConfig{}
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+	mux.HandleFunc("/metrics", apiCfg.fileserverHitsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetFileserverHitsHandler)
+	
 
 	srv := &http.Server{
 		Addr:    ":8080",
