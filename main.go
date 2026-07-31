d497c749119cb25c74706a842bcc5720e3a21163
jacobrluttrull <jacobrluttrull@gmail.com>
2026-07-31 10:56:15 -0400

    put all non-website enpoints onto the /api namespace

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "sync/atomic"
 import "fmt"
 
 type apiConfig struct {
-    fileserverHits atomic.Int32    
+    fileserverHits atomic.Int32
 }
 
 
@@ -25,21 +25,21 @@ func (cfg *apiConfig) resetFileserverHitsHandler(w http.ResponseWriter, r *http.
          w.Header().Set("Content-Type", "text/plain; charset=utf-8")
          w.WriteHeader(http.StatusOK)
          w.Write([]byte("Hits counter reset"))
-    }    
+    }
 
 func main() {
 	mux := http.NewServeMux()
 	apiCfg := &apiConfig{}
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
-	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("/api/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
-	mux.HandleFunc("/metrics", apiCfg.fileserverHitsHandler)
-	mux.HandleFunc("/reset", apiCfg.resetFileserverHitsHandler)
-	
+	mux.HandleFunc("GET /api/metrics", apiCfg.fileserverHitsHandler)
+	mux.HandleFunc("POST /api/reset", apiCfg.resetFileserverHitsHandler)
+
 
 	srv := &http.Server{
 		Addr:    ":8080",
@@ -47,4 +47,3 @@ func main() {
 	}
 	srv.ListenAndServe()
 }
-
